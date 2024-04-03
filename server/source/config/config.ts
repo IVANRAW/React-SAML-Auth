5a73102caaccaca5792bf418b7124661286887c2
IVANRAW <ivan.crudo@dune.it>
2024-04-03 15:15:58 +0200

    sso saml react + okta

--- server/source/config/config.ts
+++ server/source/config/config.ts
@@ -1,7 +1,7 @@
 const config = {
     saml: {
         cert: './source/config/saml.pem',
-        entryPoint: '',
+        entryPoint: 'https://trial-6104336.okta.com/app/trial-6104336_reactsamlintegration_1/exkcy5qfghTP4j2oe697/sso/saml',
         issuer: 'http://localhost:1337',
         options: {
             failureRedirect: '/login',
